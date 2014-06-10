f280d0014d14d17979f5ebb3f0ebd67492183ef4
dix <dix@735d13b6-9817-0410-8766-e36946ffe9aa>
2014-06-10 09:12:59 +0000

    New version number (uploaded to TER)
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/dix_xingsync/trunk@85562 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 /***************************************************************
  * Extension Manager/Repository config file for ext "dix_xingsync".
  *
- * Auto generated 07-02-2013 22:14
+ * Auto generated 10-06-2014 11:11
  *
  * Manual updates:
  * Only the data in the array - everything else is removed by next
@@ -29,19 +29,20 @@ $EM_CONF[$_EXTKEY] = array(
 	'clearCacheOnLoad' => 1,
 	'lockType' => '',
 	'author_company' => '',
-	'version' => '1.0.3',
+	'version' => '1.0.5',
 	'constraints' => array(
 		'depends' => array(
+			'typo3' => '4.5.0-6.1.99',
 			'dix_easylogin' => '0.3.2',
 			'dix_lib' => '',
 			'smarty' => '',
-			),
+		),
 		'conflicts' => array(
 		),
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:17:{s:9:"ChangeLog";s:4:"17a6";s:38:"class.tx_dixxingsync_registerLogin.php";s:4:"0993";s:35:"class.ux_tx_dixeasylogin_oauth1.php";s:4:"127f";s:12:"ext_icon.gif";s:4:"f9b3";s:17:"ext_localconf.php";s:4:"2968";s:14:"ext_tables.php";s:4:"a618";s:24:"ext_typoscript_setup.txt";s:4:"d41d";s:16:"locallang_db.xml";s:4:"06f5";s:10:"README.txt";s:4:"ee2d";s:19:"doc/wizard_form.dat";s:4:"4200";s:20:"doc/wizard_form.html";s:4:"05d0";s:32:"pi1/class.tx_dixxingsync_pi1.php";s:4:"7d35";s:17:"pi1/locallang.xml";s:4:"9e64";s:16:"static/setup.txt";s:4:"af47";s:21:"templates/import.tmpl";s:4:"765b";s:20:"templates/index.tmpl";s:4:"354e";s:25:"templates/ll_template.xml";s:4:"1be2";}',
+	'_md5_values_when_last_written' => 'a:14:{s:9:"ChangeLog";s:4:"6034";s:38:"class.tx_dixxingsync_registerLogin.php";s:4:"0993";s:35:"class.ux_tx_dixeasylogin_oauth1.php";s:4:"127f";s:12:"ext_icon.gif";s:4:"f9b3";s:17:"ext_localconf.php";s:4:"2968";s:14:"ext_tables.php";s:4:"a618";s:16:"locallang_db.xml";s:4:"06f5";s:14:"doc/manual.sxw";s:4:"48d5";s:32:"pi1/class.tx_dixxingsync_pi1.php";s:4:"7679";s:17:"pi1/locallang.xml";s:4:"9e64";s:16:"static/setup.txt";s:4:"f483";s:21:"templates/import.tmpl";s:4:"765b";s:20:"templates/index.tmpl";s:4:"354e";s:25:"templates/ll_template.xml";s:4:"1be2";}',
 	'suggests' => array(
 	),
 );
